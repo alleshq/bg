b768fa2031ef2e877f6f9af30c4c870a1edc9ae0
Archie Baer <archie@abaer.dev>
2020-06-26 17:32:04 +0100

    Config file with autostart path

--- allesbg.js
+++ allesbg.js
@@ -1,15 +1,27 @@
 const axios = require("axios");
 const wallpaper = require("wallpaper");
 const envPaths = require("env-paths");
-const imagePath = envPaths("allesbg", {suffix: ""}).data;
+const paths = envPaths("allesbg", {suffix: ""});
 const fs = require("fs");
 let url;
 
+// Check Platform
 if (!["win32", "linux", "darwin"].includes(process.platform)) {
     console.log("Not supported on this platform");
     process.exit(1);
 }
 
+// Config File
+let config;
+if (fs.existsSync(paths.config)) config = JSON.parse(fs.readFileSync(paths.config, "utf8"));
+else {
+    config = {
+        path: process.platform === "windows" ? "C:\\Program Files\\bg.exe" : "/usr/bin/bg"
+    };
+    fs.writeFileSync(paths.config, JSON.stringify(config));
+}
+
+// Get Wallpaper
 const fetchWallpaper = () => axios.get("https://bg.alles.cx/url").then(res => {
     if (url === res.data) return;
     url = res.data;
@@ -17,16 +29,17 @@ const fetchWallpaper = () => axios.get("https://bg.alles.cx/url").then(res => {
         responseType: "arraybuffer",
         timeout: 30000
     }).then(res => {
-        fs.writeFileSync(imagePath, res.data);
-        wallpaper.set(imagePath);
+        fs.writeFileSync(paths.data, res.data);
+        wallpaper.set(paths.data);
     }).catch(() => {})
 }).catch(() => {});
 
 fetchWallpaper();
 setInterval(fetchWallpaper, 60000);
 
+// Autostart
 const autoLaunch = require("auto-launch");
 if (process.pkg) (new autoLaunch({
     name: "allesbg",
-    path: process.platform === "windows" ? "C:\\Program Files\\bg.exe" : "/usr/bin/bg"
+    path: config.path
 })).enable();

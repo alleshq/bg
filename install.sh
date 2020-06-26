af7faaafa2ad65ff0389c3f66de37c29916709e0
Archie Baer <archie@abaer.dev>
2020-06-26 16:17:27 +0100

    Improved install script

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
+# Check architecture
 if [ $(uname -i) != "x86_64" ]
 then
     echo "Sorry, bg is currently only supported on x64 systems."
     exit 1
 fi
 
+# Get url
 case $(uname -s) in
-    Linux ) url="https://github.com/alleshq/bg/releases/download/1.0.0/allesbg-linux";;
-    Darwin ) url="https://github.com/alleshq/bg/releases/download/1.0.0/allesbg-macos";;
+    Linux) url="https://github.com/alleshq/bg/releases/download/1.0.0/allesbg-linux";;
+    Darwin) url="https://github.com/alleshq/bg/releases/download/1.0.0/allesbg-macos";;
     *) echo "This install script only works for Linux and macOS"; exit 1
 esac
 
-if [[ -f "/usr/bin/bg" ]]
+# Check if already installed
+if [ -f /usr/bin/bg ]
 then
     echo "You already have bg installed!"
     exit 1
 fi
 
+# Download and run
 wget -O /usr/bin/bg $url
 chmod +x /usr/bin/bg
 /usr/bin/bg &
